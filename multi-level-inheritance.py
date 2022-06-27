fca25033b6fe49e6116384afdbe9d85ec0f647f8
SujanBoopathy <sujansaradha@gmail.com>
2022-06-27 20:27:21 +0530

    python

---
+++ multi-level-inheritance.py
@@ -0,0 +1,19 @@
+class A:
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+    def getName(self):
+        return self.name
+    def getId(self):
+        return self.id
+
+class B(A):
+    pass
+
+class C(A):
+    pass
+
+o1=B('sujan',11)
+o2=C('sauba',89)
+print(o1.getId(),o1.getName())
+print(o2.getId(),o2.getName())
